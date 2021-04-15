773b11c810c304094da871d0a133512c3b966ac7
Mike Sheppard <mike@slowrush.co.uk>
2021-04-16 00:40:05 +0100

    Remove purge on post update, still keeps breaking ;(

--- kinsta-mu-plugins/cache/class-cache-purge.php
+++ kinsta-mu-plugins/cache/class-cache-purge.php
@@ -84,9 +84,7 @@ class Cache_Purge {
 
 		add_action( 'transition_post_status', array( $this, 'post_published' ), 10, 3 );
 		add_action( 'pre_post_update', array( $this, 'post_unpublished' ), 10, 2 );
-
-		add_action( 'wp_after_insert_post', array( $this, 'post_updated' ), 10, 3 );
-
+		// add_action( 'wp_after_insert_post', array( $this, 'post_updated' ), 10, 3 );
 
 		add_action( 'wp_trash_post', array( $this, 'post_trashed' ), 10 );
 
